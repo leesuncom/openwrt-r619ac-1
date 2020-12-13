7c79864a75960628ce9ade4e08c69d813c2d4fea
gr728 <62072234+gr728@users.noreply.github.com>
2020-12-13 17:41:11 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#Add one package to pacakge
+cp -rf $GITHUB_WORKSPACE/custom package/
