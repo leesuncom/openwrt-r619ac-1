1e86e4c90a176bbf09599302c93b0002686ced39
gr728 <62072234+gr728@users.noreply.github.com>
2021-02-26 21:32:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
+sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
 #Add one package to pacakge
-cp -rf $GITHUB_WORKSPACE/custom package/
+#cp -rf $GITHUB_WORKSPACE/custom package/
