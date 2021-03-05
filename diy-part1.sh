b162bb85f43a96f9fb31fddd39c8d181971948b4
gr728 <62072234+gr728@users.noreply.github.com>
2021-03-05 14:31:43 +0800

    add openclash

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
 #Add one package to pacakge
-#cp -rf $GITHUB_WORKSPACE/custom package/
+cp -rf $GITHUB_WORKSPACE/custom package/
